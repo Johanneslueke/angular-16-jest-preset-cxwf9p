81348178791d786b10534f8be5ab1751fb94749d
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-05-13 22:21:23 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,16 @@ import { IxMenu } from '@siemens/ix-angular';
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild(IxMenu) menu!: IxMenu;
-  links: Array<any> = [
-    {
-      path: 'account',
-      name: 'UserAccount',
-      icon: 'user',
-      // avatar: 'https://ui-avatars.com/api/?name=John+Doe',
-      // menu : [
-      //   {
-      //     path: 'SignIn',
-      //     name: 'SignIn',
-      //     icon: 'log-in'
-      //   }
-      //]
-    },
-    {
-      path: 'text',
-      name: 'Text Chat',
-      icon: 'mail',
-    },
-    {
-      path: 'video',
-      name: 'Video Chat',
-      icon: 'video-file',
-    },
-  ];
 
   ngAfterViewInit(): void {
     this.menu.toggleMapExpand(false);
+  }
+
+  toggle() {
+    this.menu.mapExpandChange.emit(
+      new CustomEvent('', {
+        detail: false,
+      })
+    );
   }
 }
