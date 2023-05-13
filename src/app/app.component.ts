3d1f8c09d4c98b541f3e9ff59763256b7a108836
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-05-13 23:15:45 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,24 @@ import { IxMenu } from '@siemens/ix-angular';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <ix-map-navigation
+  hideContextMenu="true"
+  application-name="Web RTC"
+  navigation-title="Test Web RTC Technology"
+  style="position: unset;"
+  #sidebar
+>
+  <ix-menu #menu [enableToggleTheme]="true"> </ix-menu>
+  <div slot="sidebar-content" style="min-height: 100vh;">
+    <button (click)="toggle()">toggle button</button>
+  </div>
+
+  <div>
+    <ng-content> </ng-content>
+  </div>
+</ix-map-navigation>
+`,
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild(IxMenu) menu!: IxMenu;
@@ -13,6 +30,9 @@ export class AppComponent implements AfterViewInit {
   }
 
   toggle() {
+    const el: HTMLElement | null = document.querySelector('#menu-collapse');
+
+    el?.click();
     this.menu.mapExpandChange.emit(
       new CustomEvent('', {
         detail: false,
